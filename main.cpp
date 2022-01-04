6a6f7ba9f5bc354bd487cffe092a70c112276cf0
Supsun <supsun-sockol@yandex.ru>
2022-01-04 16:00:59 +0300

    start to work

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    int w = 800;
+    int h = 600;
+    sf::RenderWindow window(sf::VideoMode(w, h), "4D");
+    window.setFramerateLimit(60);
+    sf::RenderTexture emptyTexture;
+    emptyTexture.create(w, h);
+    sf::Sprite emptySprite = sf::Sprite(emptyTexture.getTexture());
+    sf::Shader shader;
+    shader.loadFromFile("OutputShader.frag", sf::Shader::Fragment);
+
+    while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                {
+                    window.close();
+                }
+            }
+            window.draw(emptySprite, &shader);
+            window.display();
+        }
+        return 0;
+}
