bb30cd347ae551f32c264024398a69e648921a8c
Supsun <supsun-sockol@yandex.ru>
2022-01-05 17:00:30 +0300

    add rotation matrix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+//#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/mat4x4.hpp>
 
 int main()
 {
@@ -10,8 +16,8 @@ int main()
     float speed = 0.1f;
     bool moveKeys[6] = {false, false, false, false, false, false};
     bool rotateKeys[12] = {false, false, false, false, false, false, false, false, false, false, false, false};
-    sf::Vector3f pos = sf::Vector3f(-5.0f, 0.0f, 0.0f);
-    sf::Vector2f cumPos = sf::Vector2f(0.0, 0.0);
+    glm::vec4 pos(-5.0f, 0.0f, 0.0f, 0.0f);
+    sf::Vector2f rotation = sf::Vector2f(0.0, 0.0);
 
     sf::RenderWindow window(sf::VideoMode(w, h), "Ray tracing", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
@@ -65,18 +71,49 @@ int main()
                 else if (event.key.code == sf::Keyboard::Right) rotateKeys[3] = false;
             }
         }
-        if (moveKeys[0]) pos.x+=1* speed;
-        else if (moveKeys[2]) pos.x-=1 * speed;
-        if (moveKeys[3]) pos.y+=1 * speed;
-        else if (moveKeys[1]) pos.y-=1 * speed;
-        if (moveKeys[5]) pos.z+=1 * speed;
-        else if (moveKeys[4]) pos.z-=1 * speed;
-        if (rotateKeys[1]) cumPos.y+=1 * cumSensitivity;
-        else if (rotateKeys[0]) cumPos.y-=1 * cumSensitivity;
-        if (rotateKeys[2]) cumPos.x+=1 * cumSensitivity;
-        else if (rotateKeys[3]) cumPos.x-=1 * cumSensitivity;
-        shader.setUniform("u_mouse", cumPos);
-        shader.setUniform("u_pos", pos);
+        glm::vec4 dir(0.0f, 0.0f, 0.0f, 0.0f);
+        sf::Vector3f dirTemp;
+
+        if (rotateKeys[1]) rotation.y+=1 * cumSensitivity;
+        else if (rotateKeys[0]) rotation.y-=1 * cumSensitivity;
+        if (rotateKeys[2]) rotation.x+=1 * cumSensitivity;
+        else if (rotateKeys[3]) rotation.x-=1 * cumSensitivity;
+
+        if (moveKeys[0]) dir.x += 1;
+        else if (moveKeys[2]) dir.x -= 1;
+        if (moveKeys[3]) dir.y += 1;
+        else if (moveKeys[1]) dir.y -= 1;
+        if (moveKeys[5]) dir.z += 1;
+        else if (moveKeys[4]) dir.z -= 1;
+
+        glm::mat4 trans(1.0f);
+        glm::mat4 rotmat(1.0f);
+        rotmat[0][0] = rotmat[1][1] = cos(rotation.x);
+        rotmat[0][1] = -sin(rotation.x);
+        rotmat[1][0] = sin(rotation.x);
+        trans *= rotmat;
+        /*rotmat=glm::mat4(1.0f);
+        rotmat[0][0] = rotmat[2][2] = cos(-rotation.y);
+        rotmat[0][2] = -sin(-rotation.y);
+        rotmat[2][0] = sin(-rotation.y);
+        trans *= rotmat;*/
+        dir = trans*dir;
+        //std::cout << dir.x << "\n" << dir.y << "\n" << dir.z << "\n\n";
+
+
+        /*dirTemp.z = dir.z * cos(-rotation.y) - dir.x * sin(-rotation.y);
+        dirTemp.x = dir.z * sin(-rotation.y) + dir.x * cos(-rotation.y);
+        dirTemp.y = dir.y;
+        dir.x = dirTemp.x * cos(rotation.x) - dirTemp.y * sin(rotation.x);
+        dir.y = dirTemp.x * sin(rotation.x) + dirTemp.y * cos(rotation.x);
+        dir.z = dirTemp.z;
+        */
+
+        pos+=dir*speed;
+
+
+        shader.setUniform("u_mouse", rotation);
+        shader.setUniform("u_pos", sf::Vector3f(pos.x, pos.y, pos.z));
         window.draw(emptySprite, &shader);
         window.display();
     }
