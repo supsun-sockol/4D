26755ddb3e44414d844cdae4f3b91d815e0dc0d7
Supsun <supsun-sockol@yandex.ru>
2022-01-05 12:03:24 +0300

    some little chages whis code organization

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ int main()
     int h = 600;
     int cumX = 0;
     int cumY = 0;
-    float cumSensitivity = 0.05f;
-    float moveSensitivity = 0.1f;
-    bool keys[10] = {false, false, false, false, false, false, false, false, false, false};
+    float cumSensitivity = 0.03f;
+    float speed = 0.1f;
+    bool moveKeys[6] = {false, false, false, false, false, false};
+    bool rotateKeys[12] = {false, false, false, false, false, false, false, false, false, false, false, false};
     sf::Vector3f pos = sf::Vector3f(-5.0f, 0.0f, 0.0f);
     sf::Vector2f cumPos = sf::Vector2f(0.0, 0.0);
 
@@ -36,43 +37,44 @@ int main()
             //cumY += event.mouseMove.y - h / 2;
             else if (event.type == sf::Event::KeyPressed)
             {
-
-                if (event.key.code == sf::Keyboard::W) keys[0] = true;
-                else if (event.key.code == sf::Keyboard::A) keys[1] = true;
-                else if (event.key.code == sf::Keyboard::S) keys[2] = true;
-                else if (event.key.code == sf::Keyboard::D) keys[3] = true;
-                else if (event.key.code == sf::Keyboard::R) keys[4] = true;
-                else if (event.key.code == sf::Keyboard::F) keys[5] = true;
-                else if (event.key.code == sf::Keyboard::Up) keys[6] = true;
-                else if (event.key.code == sf::Keyboard::Down) keys[7] = true;
-                else if (event.key.code == sf::Keyboard::Left) keys[8] = true;
-                else if (event.key.code == sf::Keyboard::Right) keys[9] = true;
+                // move
+                if (event.key.code == sf::Keyboard::W) moveKeys[0] = true; //forward
+                else if (event.key.code == sf::Keyboard::A) moveKeys[1] = true; // left
+                else if (event.key.code == sf::Keyboard::S) moveKeys[2] = true; // back
+                else if (event.key.code == sf::Keyboard::D) moveKeys[3] = true; // right
+                else if (event.key.code == sf::Keyboard::R) moveKeys[4] = true; // up
+                else if (event.key.code == sf::Keyboard::F) moveKeys[5] = true; // down
+                // rotate
+                else if (event.key.code == sf::Keyboard::Up) rotateKeys[0] = true; // up (xz)
+                else if (event.key.code == sf::Keyboard::Down) rotateKeys[1] = true; // down (xz)
+                else if (event.key.code == sf::Keyboard::Left) rotateKeys[2] = true; // left (xy)
+                else if (event.key.code == sf::Keyboard::Right) rotateKeys[3] = true; // right (xy)
 
             }
             else if (event.type == sf::Event::KeyReleased)
             {
-                if (event.key.code == sf::Keyboard::W) keys[0] = false;
-                else if (event.key.code == sf::Keyboard::A) keys[1] = false;
-                else if (event.key.code == sf::Keyboard::S) keys[2] = false;
-                else if (event.key.code == sf::Keyboard::D) keys[3] = false;
-                else if (event.key.code == sf::Keyboard::R) keys[4] = false;
-                else if (event.key.code == sf::Keyboard::F) keys[5] = false;
-                else if (event.key.code == sf::Keyboard::Up) keys[6] = false;
-                else if (event.key.code == sf::Keyboard::Down) keys[7] = false;
-                else if (event.key.code == sf::Keyboard::Left) keys[8] = false;
-                else if (event.key.code == sf::Keyboard::Right) keys[9] = false;
+                if (event.key.code == sf::Keyboard::W) moveKeys[0] = false;
+                else if (event.key.code == sf::Keyboard::A) moveKeys[1] = false;
+                else if (event.key.code == sf::Keyboard::S) moveKeys[2] = false;
+                else if (event.key.code == sf::Keyboard::D) moveKeys[3] = false;
+                else if (event.key.code == sf::Keyboard::R) moveKeys[4] = false;
+                else if (event.key.code == sf::Keyboard::F) moveKeys[5] = false;
+                else if (event.key.code == sf::Keyboard::Up) rotateKeys[0] = false;
+                else if (event.key.code == sf::Keyboard::Down) rotateKeys[1] = false;
+                else if (event.key.code == sf::Keyboard::Left) rotateKeys[2] = false;
+                else if (event.key.code == sf::Keyboard::Right) rotateKeys[3] = false;
             }
         }
-        if (keys[0]) pos.x+=1* moveSensitivity;
-        else if (keys[2]) pos.x-=1 * moveSensitivity;
-        if (keys[3]) pos.y+=1 * moveSensitivity;
-        else if (keys[1]) pos.y-=1 * moveSensitivity;
-        if (keys[5]) pos.z+=1 * moveSensitivity;
-        else if (keys[4]) pos.z-=1 * moveSensitivity;
-        if (keys[7]) cumPos.y+=1 * cumSensitivity;
-        else if (keys[6]) cumPos.y-=1 * cumSensitivity;
-        if (keys[8]) cumPos.x+=1 * cumSensitivity;
-        else if (keys[9]) cumPos.x-=1 * cumSensitivity;
+        if (moveKeys[0]) pos.x+=1* speed;
+        else if (moveKeys[2]) pos.x-=1 * speed;
+        if (moveKeys[3]) pos.y+=1 * speed;
+        else if (moveKeys[1]) pos.y-=1 * speed;
+        if (moveKeys[5]) pos.z+=1 * speed;
+        else if (moveKeys[4]) pos.z-=1 * speed;
+        if (rotateKeys[1]) cumPos.y+=1 * cumSensitivity;
+        else if (rotateKeys[0]) cumPos.y-=1 * cumSensitivity;
+        if (rotateKeys[2]) cumPos.x+=1 * cumSensitivity;
+        else if (rotateKeys[3]) cumPos.x-=1 * cumSensitivity;
         shader.setUniform("u_mouse", cumPos);
         shader.setUniform("u_pos", pos);
         window.draw(emptySprite, &shader);
